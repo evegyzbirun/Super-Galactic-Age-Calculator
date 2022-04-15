264d0d43f95fde4d5c3aa914b87555951e4af5d9
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-04-15 16:46:20 -0700

    uploading in Github.

---
+++ src/js/years-factors.js
@@ -0,0 +1,10 @@
+export default class Years {
+  lifeFactors(title, lostYears) {
+    this.title = title;
+    this.lostYears = lostYears;
+  }
+
+
+}
+let factorOne = new lifeFactors("Smoking", 5) //-5
+let factorTwo = new lifeFactors("Sport", 10) // +10
