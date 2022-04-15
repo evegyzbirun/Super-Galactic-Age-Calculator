e8eabbb4e70c4fe4106b2bdb7ae4a92b5fb7bf91
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-04-15 11:40:14 -0700

    modifying index.js file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,9 @@
 // solar year 365 on Earth 
 //
-//A Mercury year is .24 Earth years.
-//A Venus year is .62 Earth years.
-//A Mars year is 1.88 Earth years.
-//A Jupiter year is 11.86 Earth years.
+//A Mercury year is .24 Earth years. mercury years age = Earth year/ Mecrucy year
+//A Venus year is .62 Earth years.  venus years age = Earth year/ venus year
+//A Mars year is 1.88 Earth years.  Mars years age = Earth year/ Mars year
+//A Jupiter year is 11.86 Earth years.  Jupiter years age = Earth year/ Jupiter year
 
 import Years from '../src/js/index.js';
 
@@ -12,10 +12,10 @@ escribe(' Years', () => {
   let age;
 
   beforeEach(() => {
-    age = new Years(3);
+    age = new Years(0, 0, 0, 0, 0);
   });
 
   test('Should return age in Mercury years', () => {
-    expect(age.inputMain).toEqual(3);
+    expect(age.earth).toEqual(age.mercury);
   });
 });
--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-export default function Years(inputMain) {
-  this.inputMain = inputMain;
+export default function Years(earth, mercury, venus, mars, jupiter) {
+  this.earth = earth;
+  this.mercury = mercury;
+  this.venus = venus;
+  this.mars = mars;
+  this.jupiter = jupiter;
 
 }
